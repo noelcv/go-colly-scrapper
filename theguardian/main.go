eadc64c8c8f27f36e0aff252f7390f268ae02ac9
noelcv <79803356+noelcv@users.noreply.github.com>
2022-08-21 17:58:50 +0200

    feat: filter 5 top results theguardian

--- theguardian/main.go
+++ theguardian/main.go
@@ -1,68 +1,3 @@
-// package main
-
-// import (
-// 	"encoding/json"
-// 	"fmt"
-// 	"log"
-// 	"os"
-
-// 	"github.com/gocolly/colly"
-// )
-
-// type item struct {
-// 	Name string `json:"name"`
-// 	Price   string `json:"price"`
-// 	ImgUrl string `json:"imgurl"`
-// }
-
-// func main() {
-// 	//instance for Colly Collector
-// 	c := colly.NewCollector(
-// 		colly.AllowedDomains("j2store.net"),
-// 	)
-	
-// 	var items []item
-	
-// 	//specify the css selector to scrape, store the Child results in your struct and append them to a slice
-// 	c.OnHTML("div.col-sm-4 div[itemprop=itemListElement]", func(h *colly.HTMLElement) {
-// 		item := item {
-// 			Name: h.ChildText("h2.product-title"),
-// 			Price: h.ChildText("div.sale-price"),
-// 			ImgUrl: h.ChildAttr("img", "src"),
-// 		}
-		
-// 		items = append(items, item)
-
-// 	})
-	
-// 	//iterate through following pages
-// 	c.OnHTML("[title=Next]", func(h *colly.HTMLElement){
-// 		next_page := h.Request.AbsoluteURL(h.Attr("href"))
-// 		c.Visit(next_page)
-// 	})
-	
-// 	//print each visited page
-// 	c.OnRequest(func(r *colly.Request){
-// 		fmt.Println(r.URL.String())
-// 	})
-	
-// 	c.Visit("https://j2store.net/demo/index.php/shop")
-	
-// 	c.OnError(func(_ *colly.Response, err error) {
-//     log.Println("Something went wrong:", err)
-// })
-
-// content, err := json.Marshal(items)
-
-// 	if err != nil {
-// 	fmt.Println(err.Error())	
-// 	}
-	
-// 	//Save file with UNIX permissions
-// 	os.WriteFile("products.json", content, 0644)
-
-// }
-
 package main
 
 import (
@@ -90,7 +25,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("could not create page: %v", err)
 	}
-	if _, err = page.Goto("https://newyorktimes.com"); err != nil {
+	if _, err = page.Goto("https://theguardian.com"); err != nil {
 		log.Fatalf("could not goto: %v", err)
 	}
 	entries, err := page.QuerySelectorAll(".fc-item__container")
