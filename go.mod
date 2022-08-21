fa0e52476ea77212977caf374b3216b70fbf0984
noelcv <79803356+noelcv@users.noreply.github.com>
2022-08-21 16:39:34 +0200

    add: playwright

--- go.mod
+++ go.mod
@@ -8,16 +8,20 @@ require (
 	github.com/antchfx/htmlquery v1.2.5 // indirect
 	github.com/antchfx/xmlquery v1.3.12 // indirect
 	github.com/antchfx/xpath v1.2.1 // indirect
+	github.com/danwakefield/fnmatch v0.0.0-20160403171240-cbb64ac3d964 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gocolly/colly v1.2.0 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/jawher/mow.cli v1.2.0 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/playwright-community/playwright-go v0.2000.1 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
 	golang.org/x/net v0.0.0-20220812174116-3211cb980234 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/square/go-jose.v2 v2.6.0 // indirect
 )
--- scraper.go
+++ scraper.go
@@ -1,66 +1,112 @@
+// package main
+
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"log"
+// 	"os"
+
+// 	"github.com/gocolly/colly"
+// )
+
+// type item struct {
+// 	Name string `json:"name"`
+// 	Price   string `json:"price"`
+// 	ImgUrl string `json:"imgurl"`
+// }
+
+// func main() {
+// 	//instance for Colly Collector
+// 	c := colly.NewCollector(
+// 		colly.AllowedDomains("j2store.net"),
+// 	)
+	
+// 	var items []item
+	
+// 	//specify the css selector to scrape, store the Child results in your struct and append them to a slice
+// 	c.OnHTML("div.col-sm-4 div[itemprop=itemListElement]", func(h *colly.HTMLElement) {
+// 		item := item {
+// 			Name: h.ChildText("h2.product-title"),
+// 			Price: h.ChildText("div.sale-price"),
+// 			ImgUrl: h.ChildAttr("img", "src"),
+// 		}
+		
+// 		items = append(items, item)
+
+// 	})
+	
+// 	//iterate through following pages
+// 	c.OnHTML("[title=Next]", func(h *colly.HTMLElement){
+// 		next_page := h.Request.AbsoluteURL(h.Attr("href"))
+// 		c.Visit(next_page)
+// 	})
+	
+// 	//print each visited page
+// 	c.OnRequest(func(r *colly.Request){
+// 		fmt.Println(r.URL.String())
+// 	})
+	
+// 	c.Visit("https://j2store.net/demo/index.php/shop")
+	
+// 	c.OnError(func(_ *colly.Response, err error) {
+//     log.Println("Something went wrong:", err)
+// })
+
+// content, err := json.Marshal(items)
+
+// 	if err != nil {
+// 	fmt.Println(err.Error())	
+// 	}
+	
+// 	//Save file with UNIX permissions
+// 	os.WriteFile("products.json", content, 0644)
+
+// }
+
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
-	"os"
 
-	"github.com/gocolly/colly"
+	"github.com/playwright-community/playwright-go"
 )
 
-type item struct {
-	Name string `json:"name"`
-	Price   string `json:"price"`
-	ImgUrl string `json:"imgurl"`
-}
-
 func main() {
-	//instance for Colly Collector
-	c := colly.NewCollector(
-		colly.AllowedDomains("j2store.net"),
-	)
-	
-	var items []item
-	
-	//specify the css selector to scrape, store the Child results in your struct and append them to a slice
-	c.OnHTML("div.col-sm-4 div[itemprop=itemListElement]", func(h *colly.HTMLElement) {
-		item := item {
-			Name: h.ChildText("h2.product-title"),
-			Price: h.ChildText("div.sale-price"),
-			ImgUrl: h.ChildAttr("img", "src"),
-		}
-		
-		items = append(items, item)
-
-	})
-	
-	//iterate through following pages
-	c.OnHTML("[title=Next]", func(h *colly.HTMLElement){
-		next_page := h.Request.AbsoluteURL(h.Attr("href"))
-		c.Visit(next_page)
-	})
-	
-	//print each visited page
-	c.OnRequest(func(r *colly.Request){
-		fmt.Println(r.URL.String())
-	})
-	
-	c.Visit("https://j2store.net/demo/index.php/shop")
-	
-	c.OnError(func(_ *colly.Response, err error) {
-    log.Println("Something went wrong:", err)
-})
-
-content, err := json.Marshal(items)
-
+	pw, err := playwright.Run()
 	if err != nil {
-	fmt.Println(err.Error())	
+		log.Fatalf("could not start playwright: %v", err)
 	}
-	
-	//Save file with UNIX permissions
-	os.WriteFile("products.json", content, 0644)
-
+	browser, err := pw.Chromium.Launch()
+	if err != nil {
+		log.Fatalf("could not launch browser: %v", err)
+	}
+	page, err := browser.NewPage()
+	if err != nil {
+		log.Fatalf("could not create page: %v", err)
+	}
+	if _, err = page.Goto("https://theguardian.com"); err != nil {
+		log.Fatalf("could not goto: %v", err)
+	}
+	entries, err := page.QuerySelectorAll(".fc-item__container")
+	if err != nil {
+		log.Fatalf("could not get entries: %v", err)
+	}
+	for i, entry := range entries {
+		titleElement, err := entry.QuerySelector("a[data-link-name=article]")
+		if err != nil {
+			log.Fatalf("could not get title element: %v", err)
+		}
+		title, err := titleElement.TextContent()
+		if err != nil {
+			log.Fatalf("could not get text content: %v", err)
+		}
+		fmt.Printf("%d: %s\n", i+1, title)
+	}
+	if err = browser.Close(); err != nil {
+		log.Fatalf("could not close browser: %v", err)
+	}
+	if err = pw.Stop(); err != nil {
+		log.Fatalf("could not stop Playwright: %v", err)
+	}
 }
-
-
